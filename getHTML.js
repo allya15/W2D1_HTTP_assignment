26d32e4c65e4661d6d66dfd2ef221c9a54ca52ec
Ally <allison.avis@gmail.com>
2018-10-31 02:12:29 +0000

    scripting HTTP

--- getHTML.js
+++ getHTML.js
@@ -1,25 +1,36 @@
 var https = require('https');
 
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step2.html'
-};
 
-function getAndPrintHTML(callback) {
 
+function getHTML (options, callback) {
   https.get(requestOptions, function (response) {
-    if (response.statusCode !== 200) {
+
+    if  (response.statusCode !== 200) {
       console.log("Error loading page: ", response.statuscode);
     }
+
     response.setEncoding('utf8');
+
     var dataStream = [];
+
     response.on('data', function (data) {
       dataStream.push(data);
-      // console.log('Chunk Received. Content: \n' + data + "\n");
     });
+
     response.on('end', function () {
-      console.log(dataStream + "\n");
+      return printHTML(dataStream);
     });
+
   });
 }
-getAndPrintHTML();
+
+function printHTML (dataStream) {
+  console.log(dataStream + "\n");
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
